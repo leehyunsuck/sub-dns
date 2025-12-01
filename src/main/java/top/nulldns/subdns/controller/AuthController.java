77e9e203570fe9da66fb5880a126d7a2ce5e9498
leehyunsuck <leehyunsuck01@gmail.com>
2025-12-02 04:08:35 +0900

    add Auth class

---
+++ src/main/java/top/nulldns/subdns/controller/AuthController.java
@@ -0,0 +1,34 @@
+package top.nulldns.subdns.controller;
+
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import top.nulldns.subdns.entity.Member;
+import top.nulldns.subdns.service.AuthService;
+
+@Controller
+@RequiredArgsConstructor
+public class AuthController {
+    private final AuthService authService;
+
+    @GetMapping("/login-success")
+    public String loginSuccess(OAuth2AuthenticationToken token, HttpSession session) {
+        String provider = token.getAuthorizedClientRegistrationId();
+        String providerId = token.getPrincipal().getAttribute("id").toString();
+
+        Member member = authService.loginOrSignup(provider, providerId);
+        session.setAttribute("id", member.getProvider() + member.getProviderId());
+        session.setAttribute("privateId", member.getId());
+
+        return "redirect:/";
+    }
+
+    @PostMapping("/logout")
+    public String logout(HttpSession session) {
+        session.invalidate();
+        return "redirect:/";
+    }
+}
---
+++ src/main/java/top/nulldns/subdns/controller/AuthRestController.java
@@ -0,0 +1,20 @@
+package top.nulldns.subdns.controller;
+
+import jakarta.servlet.http.HttpSession;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import top.nulldns.subdns.dto.IdDto;
+
+@RestController
+@RequestMapping("/api")
+public class AuthRestController {
+    @GetMapping("/me")
+    public ResponseEntity<IdDto> me(HttpSession session) {
+        String id = (String) session.getAttribute("id");
+
+        return id == null ? ResponseEntity.status(HttpStatus.UNAUTHORIZED).build() : ResponseEntity.ok(IdDto.builder().id(id).build());
+    }
+}

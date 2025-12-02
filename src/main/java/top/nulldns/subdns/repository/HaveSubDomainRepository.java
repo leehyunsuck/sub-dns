3d05271ffec176bc46fedb9ffb2567adfb0c5f4d
leehyunsuck <leehyunsuck01@gmail.com>
2025-12-02 16:12:15 +0900

    feat: get count method - have sub domain count

--- src/main/java/top/nulldns/subdns/repository/HaveSubDomainRepository.java
+++ src/main/java/top/nulldns/subdns/repository/HaveSubDomainRepository.java
@@ -9,4 +9,6 @@ import java.util.Optional;
 public interface HaveSubDomainRepository extends JpaRepository<HaveSubDomain, Long> {
     List<HaveSubDomain> findByMemberId(Long memberId);
     Optional<HaveSubDomain> findByMemberIdAndFullDomain(Long memberId, String fullDomain);
+
+    int countByMemberId(Long memberId);
 }

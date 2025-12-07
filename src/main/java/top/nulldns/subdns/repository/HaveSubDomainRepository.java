0b74089a2f7545ac580fd4c488549ed01f53d32f
leehyunsuck <leehyunsuck01@gmail.com>
2025-12-07 10:39:55 +0900

    feat: 쿼리 추가

--- src/main/java/top/nulldns/subdns/repository/HaveSubDomainRepository.java
+++ src/main/java/top/nulldns/subdns/repository/HaveSubDomainRepository.java
@@ -1,14 +1,26 @@
 package top.nulldns.subdns.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import top.nulldns.subdns.entity.HaveSubDomain;
+import org.springframework.data.jpa.repository.Query;
+import top.nulldns.subdns.dao.HaveSubDomain;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface HaveSubDomainRepository extends JpaRepository<HaveSubDomain, Long> {
     List<HaveSubDomain> findByMemberId(Long memberId);
-    Optional<HaveSubDomain> findByMemberIdAndFullDomain(Long memberId, String fullDomain);
 
-    int countByMemberId(Long memberId);
+    @Query("SELECT h FROM HaveSubDomain h WHERE h.member.id = :memberId GROUP BY h.fullDomain")
+    List<HaveSubDomain> findDistinctByMemberId(Long memberId);
+
+    List<HaveSubDomain> findAllByMemberIdAndFullDomain(Long memberId, String fullDomain);
+    Optional<HaveSubDomain> findByMemberIdAndFullDomainAndRecordType(Long memberId, String fullDomain, String recordType);
+
+    @Query("select count(distinct h.fullDomain) from HaveSubDomain h where h.member.id = :memberId")
+    int countDistinctFullDomainByMemberId(Long memberId);
+
+    boolean existsHaveSubDomainByMemberIdAndFullDomain(Long memberId, String fullDomain);
+
+    boolean existsByFullDomain(String fullDomain);
+
 }
